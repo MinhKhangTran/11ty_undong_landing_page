4326205d3b94b2999febf00860054f36d60321e5
MinhKhangTran <minhkhang.tran@gmx.de>
2022-10-10 05:19:58 +0200

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,23 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addWatchTarget("./src/scss/");
+
+  // add filter for the year
+  eleventyConfig.addFilter("year", () => {
+    return new Date().getFullYear();
+  });
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site",
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  };
+};
